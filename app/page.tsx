f0e3f9563f9ab801cf93ced23ed09c5b2303879c
Anshuman Rai <118046194+anshuman-rai-27@users.noreply.github.com>
2024-09-30 18:47:50 +0530

    update

--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,7 @@ import Hero from "@/components/hero";
 import HomePage from "@/components/home/home";
 import DopingImpact from "@/components/home/impacts";
 import AthleteToolkit from "@/components/home/info";
+import { CoverDemo } from "@/components/home/landing";
 import Leader from "@/components/home/leader";
 import NewsSection from "@/components/home/news";
 import TestimonialCarousel from "@/components/home/test";
@@ -21,12 +22,26 @@ import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
 import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
 import { Compare } from "@/components/ui/compare";
 import { hasEnvVars } from "@/utils/supabase/check-env-vars";
-
+import Image from "next/image";
+// bg-gradient-to-r from-blue-500 to-purple-700 block
+// bg-opacity-40 backdrop-filter backdrop-blur-sm bg-black
 export default async function Index() {
   return (
     <>
       <NavBar absoluteLago={true} />
-      <div className="h-[100vh] bg-gradient-to-r from-blue-500 to-purple-700 block">dsfsdklkfnkl</div>
+      <div className="h-[100vh] ">
+      <Image
+      src="/bg.jpg"
+      width={1600}
+      height={637}
+      alt="Picture of the author"
+      className="w-full h-[100vh] absolute"
+    />
+    <div className="absolute left-[10vw] top-[20vw] text-white">
+    <CoverDemo/>
+    </div>
+    
+      </div>
       <TestimonialCarousel/>
       {/* <BackgroundXO/> */}
       {/* <Meteors/> */}
@@ -42,7 +57,7 @@ export default async function Index() {
       <NewsSection/>
       <CompareDemo/>
       
-      <TextGenerateEffectDemo/>
+      {/* <TextGenerateEffectDemo/> */}
       <Footer/>
       {/* <DopingImpact/> */}
       {/* <FocusCardsDemo/> */}
--- components/common/Nav.tsx
+++ components/common/Nav.tsx
@@ -17,7 +17,7 @@ export default function NavBar({ absoluteLago }: { absoluteLago: boolean }) {
 
   return (
     <nav
-      className={`w-[100%] flex  justify-between ${absoluteLago && "absolute"} backdrop-blur-xl backdrop:bg-slate-600 z-10  px-5 py-2`}
+      className={`w-[100%] flex  justify-between ${absoluteLago && "absolute"}  z-10  px-5 `}
       // style={isHomePage ? {} : { background: 'linear-gradient(90deg, rgba(195,196,5,1) 8%, rgba(9,121,24,1) 52%, rgba(14,177,46,1) 100%)' }}
     >
       <div className="flex flex-col justify-center items-center w-[100px]">
