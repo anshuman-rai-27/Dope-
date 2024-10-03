b17835e07d945879d0766bb8435d52f08fc9cda4
Anshuman Rai <118046194+anshuman-rai-27@users.noreply.github.com>
2024-10-03 21:37:58 +0530

    game_link

--- components/home/landing.tsx
+++ components/home/landing.tsx
@@ -1,6 +1,7 @@
 "use client"
 import React from "react";
 import { Cover } from "@/components/ui/cover";
+import Link from "next/link";
 
 export function CoverDemo() {
     return (
@@ -9,7 +10,10 @@ export function CoverDemo() {
         Build amazing websites <br /> at <Cover>warp speed</Cover>
       </h1> */}
             <h1 className="text-4xl md:text-4xl lg:text-6xl font-semibold max-w-7xl mx-auto text-center mt-6 relative z-20 py-6 bg-clip-text text-transparent bg-gradient-to-b from-white via-white to-white dark:from-white dark:via-white dark:to-white">
-            Gamified Platform to Promote Anti-Doping and Clean Sports <Cover>Play</Cover>
+            Gamified Platform to Promote Anti-Doping and Clean Sports 
+            <Link href={'https://dope-cop-host.vercel.app/'}>
+            <Cover>Play</Cover>
+            </Link>
             </h1>
 
         </div>
